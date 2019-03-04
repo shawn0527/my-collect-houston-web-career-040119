d20de4f51637186b4914ad409c936460bf3f8cc9
Shawn Ren <doggy-ren@hotmail.com>
2019-03-04 19:36:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
-
-
+def my_collect (array)
+  i = 0
+  newArray = []
+  if array == []
+    while i < array.length
+      i += 1
+    end
+  elsif array[i].include? " "
+    while i < array.length
+      yield array[i].split(" ").first
+      newArray.push(array[i].split(" ").first)
+      i += 1
+    end
+  else
+    while i < array.length
+      yield array[i].upcase
+      newArray << array[i].upcase
+      i += 1
+    end
+  end
+  newArray
+end
